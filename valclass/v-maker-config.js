4b03981c1fbdbf57768f1669e412d7b53439c10b
Muhammad Khoirul Huda <79193720+valclassdevelop@users.noreply.github.com>
2021-11-15 17:05:21 +0700

    Add files via upload

---
+++ valclass/v-maker-config.js
@@ -0,0 +1,32 @@
+createMakerUtilitiesName({
+    createUtilities : [
+        "greenButton",
+        "greenText",
+        "darkText",
+        "radiusNumber",
+        "clip",
+        "clip2",
+        "clipBgColor",
+        "clipBgColor2",
+      ],
+    createValueUtilities : [
+        "background-color: rgb(21, 255, 0);",
+        "color: rgb(21, 255, 0);",
+        "color: rgb(23, 26, 71);",
+        "border-radius: 0 5px 5px 0;",
+        "clip-path: polygon(0 10%, 100% 0, 100% 100%, 0 91%);",
+        "clip-path: polygon(0 0, 100% 7%, 100% 100%, 0 100%);",
+        "background-color: rgb(212, 215, 255);",
+        "background-color: rgba(152, 159, 255, 0.897);",
+      ],
+    createBreakPointsUtilities : [
+        "df",
+        "df",
+        "df",
+        "df",
+        "df",
+        "df",
+        "df",
+        "df",
+      ],
+})
